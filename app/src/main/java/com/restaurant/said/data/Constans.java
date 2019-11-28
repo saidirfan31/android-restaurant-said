2a18054adaaa75a8e87787d576b836116ce2e0c9
Muhammad Said Marzuqi <saidirfan31@gmail.com>
2019-11-28 07:19:20 +0700

    login register

---
+++ app/src/main/java/com/restaurant/said/data/Constans.java
@@ -0,0 +1,10 @@
+package com.restaurant.said.data;
+
+public class Constans {
+    ///Restoran
+    public static final String WEB_URL_RESTORAN =
+            "http://www.pakartanaman.com/skripsi/public/";
+    public static final String REGISTER = WEB_URL_RESTORAN +
+            "register/";
+    public static final String LOGIN = WEB_URL_RESTORAN + "login/";
+}
